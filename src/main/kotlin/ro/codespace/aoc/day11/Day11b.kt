16c734a1e26e55d77769211f48042d197a783b24
Andrei Gavrila <andreigavrila1401@gmail.com>
2019-12-11 07:50:54 +0200

    day 11 and prep day 12.
    Messed up turning left and fked up the first bit

--- src/main/kotlin/ro/codespace/aoc/day11/Day11b.kt
+++ src/main/kotlin/ro/codespace/aoc/day11/Day11b.kt
@@ -1,9 +1,60 @@
 package ro.codespace.aoc.day11
 
+import ro.codespace.aoc.Program
+import ro.codespace.aoc.Vector2
 import ro.codespace.aoc.day
 
 
 fun main() {
-    val code = day(11).readLine().split(",").map { it.toInt() }
+    val code = day(11).readLine().split(",").map { it.toLong() }
+
+
+    val painted = mutableMapOf<Vector2, Int>()
+
+    var currentPos = Vector2(0, 0)
+    var currentDir = Vector2(0, -1)
+
+    painted[currentPos] = 1
+
+    val reader = {
+        painted.getOrDefault(currentPos, 0)
+    }
+
+    var outputCounter = 0
+
+    val output: (String) -> Unit = {
+        if (outputCounter % 2 == 0) {
+            painted[currentPos] = it.toInt()
+        } else {
+            when (it.toInt()) {
+                0 -> currentDir = Vector2(currentDir.y, -currentDir.x)
+                1 -> currentDir = Vector2(-currentDir.y, currentDir.x)
+            }
+            currentPos += currentDir
+        }
+        outputCounter++
+    }
+    Program(code, reader, output)()
+
+
+    println()
+    for (y in -10..10) {
+        for (x in -50..50) {
+            if (y != 0 || x != 0) {
+                val color = painted.getOrDefault(Vector2(currentPos.x + x, currentPos.y + y), 0)
+                print(if (color == 1) "#" else " ")
+            } else {
+                print(when (currentDir) {
+                    Vector2(0, -1) -> "^"
+                    Vector2(0, 1) -> "v"
+                    Vector2(-1, 0) -> "<"
+                    Vector2(1, 0) -> ">"
+                    else -> ""
+                })
+            }
+        }
+        println()
+    }
+    println()
 
 }
---
+++ src/main/kotlin/ro/codespace/aoc/day12/Day12b.kt
@@ -0,0 +1,8 @@
+package ro.codespace.aoc.day12
+
+import ro.codespace.aoc.day
+
+
+fun main() {
+    day(12)
+}
